e3f1e3080c9cc73d3ef8bfe383927f2b8d584ee0
d8uv <d8uv@d8uv.org>
2010-07-09 12:24:39 -0800

    tooltips are a work

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
+
+    /* This is seriously all there is to the password generatin'.
+       Just concatenate the two fields. Perform an MD5 on what you get.
+       Then, convert the base16 into base64. Any fool can recreate this. */
+
     $("form").keyup(function() {
         $("#out").text(Base64ize(MD5(input.in1.value + input.in2.value)));
+    });
+
+
+
+    $("p").hide().prev().append("<span class='tip'>?</span>");
+    $(".tip").click(function () {
+	$(this).parent().next().slideToggle("slow");
     });
 });
