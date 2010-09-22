3104bd40b12f7d2e97812b2508b9fae2656cd0ce
d8uv <d8uv@d8uv.org>
2010-09-22 11:05:11 -0800

    Try it again, this time with feeling.

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 $(document).ready(function () {
-
-    /* This is seriously all there is to the password generatin'.
-       Just concatenate the two fields. Perform an MD5 on what you get.
-       Then, convert the base16 into base64. Any fool can recreate this. */
-
     $("form").keyup(function() {
         $("#out").text(Base64ize(MD5(input.in1.value + input.in2.value)));
-    });
-
-
-
-    $(".tip").hide().prev().append("<span class='tipToggle'>?</span>");
-    $(".tipToggle").click(function () {
-	$(this).parent().next().slideToggle("slow");
     });
 });
